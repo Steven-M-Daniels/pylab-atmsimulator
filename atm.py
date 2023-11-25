f9f68557dcd49b4f7bd06c9b333017081f55b774
Steven-M-Daniels <148572456+Steven-M-Daniels@users.noreply.github.com>
2023-11-25 11:30:08 -0600

    Update atm.py
    Updated to utilize functions to clean up code, as well as add error checking to avoid negative amount transactions and avoid errors when non-number inputs are entered on amounts.

--- atm.py
+++ atm.py
@@ -1,6 +1,46 @@
+def deposit_money(account, balance):
+    while True:
+        print()
+        try:
+            amount = float(input(f"Amount to add to {account}: $"))
+        except ValueError:
+            print("Value must be a number!")
+            continue
+        else:
+            if amount < 0:
+                print("Amount must be a positive number.")
+                continue
+            else:
+                balance += amount
+                break
+            break
+    return balance
+
+
+def withdraw_money(account, balance):
+    while True:
+        print()
+        try:
+            amount = float(input(f"Amount to take from {account}: $"))
+        except ValueError:
+            print("Value must be a number!")
+            continue
+        else:
+            if balance - amount < 0:
+                print("Not enough money in account!")
+                continue
+            elif amount < 0:
+                print("Amount must be a positive number.")
+                continue
+            else:
+                balance -= amount
+                break
+            break
+    return balance
+
+
 checking = 0.00
 savings = 0.00
-amount = 0.00
 
 print("Welcome to the ATM Simulator!")
 print()
@@ -13,35 +53,17 @@ while True:
     if menu_selection == "d":
         menu_selection = input("(C)hecking or (S)avings? ").lower()
         if menu_selection == "c":
-            print()
-            amount = float(input("Amount to add to Checking: $"))
-            checking += amount
+            checking = deposit_money("Checking", checking)
         elif menu_selection == "s":
-            print()
-            amount = float(input("Amount to add to Savings: $"))
-            savings += amount
+            savings = deposit_money("Savings", savings)
         else:
             print("Incorrect input detected.")
     elif menu_selection == "w":
         menu_selection = input("(C)hecking or (S)avings? ").lower()
         if menu_selection == "c":
-            while True:
-                print()
-                amount = float(input("Amount to take from Checking: $"))
-                if checking - amount >= 0:
-                    checking -= amount
-                    break
-                else:
-                    print("Not enough money in account!")
+            checking = withdraw_money("Checking", checking)
         elif menu_selection == "s":
-            while True:
-                print()
-                amount = float(input("Amount to take from Savings: $"))
-                if savings - amount >= 0:
-                    savings -= amount
-                    break
-                else:
-                    print("Not enough money in account!")
+            savings = withdraw_money("Savings", savings)
         else:
             print("Incorrect input detected.")
     elif menu_selection == "q":
